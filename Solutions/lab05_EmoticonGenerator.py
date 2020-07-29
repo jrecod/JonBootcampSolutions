06994266e93683a8ac534cfafd83a36d5d94d0fa
jrecod <68835924+jrecod@users.noreply.github.com>
2020-07-29 14:32:51 -0700

    Delete lab05_EmoticonGenerator.py

--- Solutions/lab05_EmoticonGenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-eyes = [':', ';', '8', '=']
-nose = ['-', '^', ' ']
-mouth = [']', '}', '[', '{', '(', ')', '@', '3', 'D']
-
-emoticon = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
-
-print(emoticon)
