05ecc9c3175fa12eea3d64d31477398e3f223f40
jrecod <recodj@gmail.com>
2020-08-05 22:23:34 -0700

    trimmed up

--- Solutions/lab03-grading.py
+++ Solutions/lab03-grading.py
@@ -1,10 +1,9 @@
 # Grading
 
-# prompt user to enter score
-score = input("Enter your score: ")
+# prompt user to enter score and convert the string number to an integer number
+score = int(input("Enter your score: "))
 
-#convert the string number to an integer number
-score = int(score)
+
 
 # compare the score to the grade
 if score >= 90 and score <= 100:
@@ -15,7 +14,7 @@ elif score >= 70 and score <= 79:
     result = f'You got a C'
 elif score >= 60 and score <= 69:
     result = f'You got a D'
-elif score >= 0 and score <= 59:
+else:
     result = f'You got a F'
 
 # display the result
