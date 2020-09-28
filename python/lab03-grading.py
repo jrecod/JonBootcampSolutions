c0cd33aed51febe03c95235e733ee4e7b0f6450c
jrecod <recodj@gmail.com>
2020-09-28 14:16:10 -0700

    Finished ver 2

--- python/lab03-grading.py
+++ python/lab03-grading.py
@@ -2,18 +2,34 @@
 
 # prompt user to enter score and convert the string number to an integer number
 score = int(input("Enter your score: "))
-
+qualifier = ''
 
 
 # compare the score to the grade
 if score >= 90 and score <= 100:
-    result = f'You got an A'
+    if score % 10 <= 4:
+        qualifier = '-'
+    elif score % 10 >= 6:
+        qualifier = '+'
+    result = f'You got an A{qualifier}'
 elif score >= 80 and score <= 89:
-    result = f'You got a B'
+    if score % 10 <= 4:
+        qualifier = '-'
+    elif score % 10 >= 6:
+        qualifier = '+'
+    result = f'You got a B{qualifier}'
 elif score >= 70 and score <= 79:
-    result = f'You got a C'
+    if score % 10 <= 4:
+        qualifier = '-'
+    elif score % 10 >= 6:
+        qualifier = '+'
+    result = f'You got a C{qualifier}'
 elif score >= 60 and score <= 69:
-    result = f'You got a D'
+    if score % 10 <= 4:
+        qualifier = '-'
+    elif score % 10 >= 6:
+        qualifier = '+'
+    result = f'You got a D{qualifier}'
 else:
     result = f'You got a F'
 
