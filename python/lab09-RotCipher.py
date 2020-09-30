87170b58887b93b4a9f16fb92d32b5b8e87cfc96
jrecod <recodj@gmail.com>
2020-09-29 17:43:31 -0700

    Finished version 2

--- python/lab09-RotCipher.py
+++ python/lab09-RotCipher.py
@@ -3,10 +3,11 @@ def rot13(phrase):
     out_phrase = ''
 
     for char in phrase:
-        out_phrase += a[(a.find(char)+13)%len(a)]
+        out_phrase += a[(a.find(char)+(rotation))%len(a)]
     return out_phrase
 
 phrase = input('Please enter a phrase you would like to have ciphered: ')
+rotation = int(input('Please enter the amount of times you would like to rotate: '))
 print(rot13(phrase))
 
 #add another function that asks for that takes input for rotation amount
