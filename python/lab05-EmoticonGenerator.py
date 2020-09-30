71047c6367d4752913a29830d54304f26a4ee69a
jrecod <recodj@gmail.com>
2020-09-29 17:35:11 -0700

    Version 3 done

--- python/lab05-EmoticonGenerator.py
+++ python/lab05-EmoticonGenerator.py
@@ -4,6 +4,28 @@ eyes = [':', ';', '8', '=']
 nose = ['-', '^', ' ']
 mouth = [']', '}', '[', '{', '(', ')', '@', '3', 'D']
 
-emoticon = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
 
-print(emoticon)
+i = 0
+while i < 5:
+    emoticon = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
+    i += 1
+    print(emoticon)
+
+
+vert_eyes_list = ['-', 'O', 'o', '0', 'X', 'x', '~', '*']
+vert_mouth_list = ['_', '.', '>', 'u']
+sides_list = [
+    ['[', ']'],
+    ['(', ')'],
+    ['{', '}'],
+    ['<([', '])>'],
+]
+
+
+sides = random.choice(sides_list)
+vert_eyes = random.choice(vert_eyes_list)
+vert_mouth = random.choice(vert_mouth_list)
+
+vert_emoticon = sides[0] + vert_eyes + vert_mouth + vert_eyes + sides[1]
+
+print(vert_emoticon)
